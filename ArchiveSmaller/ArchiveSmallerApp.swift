709160dff3053dad2601b8401fa46afcdcf6faf0
Dylan Southard <southard.dylan@gmail.com>
2023-06-11 12:18:29 +0700

    Initial Commit

---
+++ ArchiveSmaller/ArchiveSmallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveSmallerApp.swift
+//  ArchiveSmaller
+//
+//  Created by Dylan Southard on 2023-06-11.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveSmallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
